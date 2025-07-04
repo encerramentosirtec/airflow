4034f129e25fa6627532221bfaf8e17a9b29693e
HeliNeres <helineres@hotmail.com>
2025-07-04 14:48:27 -0300

    correção airflow

--- start_airflow.sh
+++ start_airflow.sh
@@ -4,11 +4,8 @@ WORKDIR="/home/sirtec-fechamento/airflow"
 VENV="$WORKDIR/airenv/bin/activate"
 LOG_FILE="$WORKDIR/airflow.log"
 
-# Ativa o ambiente virtual (se existir)
+# Ativa o ambiente virtual
 source "$VENV"
 
-# Limpa arquivos antigos
-> "$LOG_FILE"
-
-# Inicia o airflow em background e redireciona a saída
-airflow standalone > "$LOG_FILE" 2>&1 &
+# Inicia o Airflow Standalone e salva o log
+airflow standalone > "$LOG_FILE" 2>&1
