cb2814ddb49d1810b2f786399dd3919468c771a5
hugoviana95 <hugovasantos@gmail.com>
2025-09-13 17:18:28 -0300

    Alteração na dag das UARs

--- dags/manut/atualiza_uar.py
+++ dags/manut/atualiza_uar.py
@@ -1,9 +1,10 @@
 from airflow.sdk import DAG
-from airflow.providers.standard.operators.python import PythonOperator
+from airflow.providers.standard.operators.python import PythonOperator, ShortCircuitOperator
 import os
 import pandas as pd
 import pendulum 
 import sys
+from datetime import datetime, timedelta
 
 
 PATH = os.getenv('AIRFLOW_HOME')
@@ -20,15 +21,36 @@ DRIVE = GoogleDrive()
 
 
 
+def verifica_alteracao_arquivos(**context):
+    ultima_execucao = context['prev_data_interval_start_success']
+    ultima_atualizacao = [x['modifiedTime'] for x in DRIVE.listar_arquivos() if x['name'] == 'uar'][0]
+
+    if ultima_execucao is None:
+        return True
+
+    datetime_ultima_atualizacao = pendulum.parse(ultima_atualizacao)
+    
+    print('Última atualização:', datetime_ultima_atualizacao)
+    print('Última execução:', ultima_execucao)
+
+    atualizar = datetime_ultima_atualizacao > ultima_execucao
+
+    if atualizar:
+        print("Arquivos foram atualizados. Iniciando atualização da base de UAR.")
+    
+    return atualizar
+
+
+
 def atualizar_base_uar():
     """
         Atualiza a base de UAR
     """
     try:
         # Baixa o arquivo do Google Drive
-        drive = GoogleDrive()
-        drive.baixar_arquivo('uar_atualizado.txt')
-        drive.baixar_arquivo('uar_ativo.txt')
+        pasta_uar = DRIVE.buscar_pasta_por_nome('uar')
+        DRIVE.baixar_arquivo('uar_atualizado.txt', arquivo_id=pasta_uar)
+        DRIVE.baixar_arquivo('uar_ativo.txt', arquivo_id=pasta_uar)
 
         # Lê o arquivo baixado
         df_ativo = pd.read_csv(os.path.join(PATH, 'downloads/uar_ativo.txt'), sep='#', encoding='ISO-8859-1', skiprows=3, on_bad_lines='warn')
@@ -55,6 +77,11 @@ def atualizar_base_uar():
         raise e
 
 
+if __name__ == '__main__':
+    fake_context = {'prev_data_interval_start_success': '2025-09-12T00:00:00+00:00'}
+    verifica_alteracao_arquivos(**fake_context)
+
+
 
 default_args = {
     'depends_on_past' : False,
@@ -70,15 +97,19 @@ default_args = {
 with DAG(
     dag_id='atualiza_uar',
     tags=['manut'],
-    schedule='0 8,15 * * 1-5',
+    schedule='*/1 7-22 * * *',
     default_args=default_args,
     start_date=pendulum.today('America/Sao_Paulo')
 ):
 
+    checar_alteracao_arquivos = ShortCircuitOperator(
+        task_id='checar_alteracao_arquivos',
+        python_callable=verifica_alteracao_arquivos,
+    )
+
     atualiza_uar = PythonOperator(
         task_id='atualiza_uar',
         python_callable=atualizar_base_uar,
-
     )
 
-    atualiza_uar
+    checar_alteracao_arquivos >> atualiza_uar
