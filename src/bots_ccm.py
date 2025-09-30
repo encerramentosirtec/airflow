590b97e609d3fed4a374bf12173f3ef08f8b6e0e
HeliNeres <helineres@hotmail.com>
2025-09-30 15:00:14 -0300

    Fix condition to check project status and handle non-digit project IDs in bots_ccm.py

--- src/bots_ccm.py
+++ src/bots_ccm.py
@@ -240,7 +240,8 @@ class Bots:
         ####################### CONFERE QUAIS OBRAS JÁ ESTÃO NA PLANILHA DO FECHAMENTO
         cont = 0
         for cont, i in enumerate(obras_concluidas): 
-            if i in obras_recepcionadas_geral or espelho_CCM.loc[espelho_CCM["PROJETO"] == int(i), "STATUS GERAL"].values[0] != 'CANCELADA':
+            mask = espelho_CCM["PROJETO"] == int(i) if str(i).isdigit() else False
+            if (i in obras_recepcionadas_geral) or (mask.any() and espelho_CCM.loc[mask, "STATUS GERAL"].iloc[0] != "CANCELADA"):
                 pass
             else:
                 obras_concluidas_sem_pasta_no_fechamento.append(obras_concluidas[cont])
