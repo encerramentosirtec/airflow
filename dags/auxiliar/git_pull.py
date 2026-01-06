93a0de15ccdf2e04375324027c0aca2386a5d2c3
HeliNeres <helineres@hotmail.com>
2026-01-06 07:19:45 -0300

    Adiciona DAG 'executar_git_pull' para sincronizar repositório usando git pull

--- dags/auxiliar/git_pull.py
+++ dags/auxiliar/git_pull.py
@@ -2,11 +2,6 @@ from airflow.sdk import DAG
 from airflow.providers.standard.operators.bash import BashOperator
 from pendulum import today
 
-# Configurações
-REPO_URL = "github.com/seu-usuario/seu-repositorio.git"
-GIT_TOKEN = "{{ var.value.git_token }}"
-LOCAL_PATH = "/tmp/meu_projeto_git"
-
 with DAG(
     'executar_git_pull',
     start_date=today('America/Sao_Paulo'),
@@ -18,7 +13,7 @@ with DAG(
         task_id='sync_repo',
         bash_command=f"""
             cd ~/airflow
-            git push
+            git pull
         """
     )
 
