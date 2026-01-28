591cc0ba8c3e04f88d9c187d507315a9ae0bf711
hugoviana95 <hugovasantos@gmail.com>
2026-01-28 13:21:47 -0300

    Ajuste no último commit

--- dags/manut/atualiza_medicoes.py
+++ dags/manut/atualiza_medicoes.py
@@ -82,7 +82,7 @@ def atualizar_base_medicoes():
         'STATUS AJUSTADO': 'first',
         'ID_MEDIÇÃO': 'first',
         'VALOR_PREVISTO': 'sum'
-    }).sort_values(by=['STATUS AJUSTADO', 'ID'], ascending=True)
+    }).sort_values(by=['STATUS AJUSTADO', 'ID'], ascending=[True, False])
 
 
     ### Atualização da base
