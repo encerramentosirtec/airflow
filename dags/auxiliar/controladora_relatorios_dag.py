03ba42aca093188bf327c1b169692000e102a93a
HeliNeres <helineres@hotmail.com>
2026-04-23 07:52:23 -0300

    feat: atualizar cronograma da DAG controladora_relatorios para executar a cada 30 minutos

--- dags/auxiliar/controladora_relatorios_dag.py
+++ dags/auxiliar/controladora_relatorios_dag.py
@@ -31,7 +31,7 @@ default_args = {
 with DAG('controladora_relatorios',
         default_args = default_args,
         start_date=datetime(2026, 4, 17, tzinfo=timezone("America/Sao_Paulo")),
-        schedule = '0 7-18 * * 1-6',
+        schedule = '0,30 7-18 * * 1-6',
         tags = ['aux', 'relatorios', 'stc', 'email', 'geoex'],
         catchup = False,
         on_failure_callback=[
